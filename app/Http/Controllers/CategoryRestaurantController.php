5c2a523400bac09433f8f856ea79344c864f3825
GiuseppeDeRosa1992 <derosagiuseppe1092@gmail.com>
2024-09-19 11:41:16 +0200

    added seeder and models

---
+++ app/Http/Controllers/CategoryRestaurantController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category_Restaurant;
+use Illuminate\Http\Request;
+
+class CategoryRestaurantController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Category_Restaurant $category_Restaurant)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Category_Restaurant $category_Restaurant)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Category_Restaurant $category_Restaurant)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Category_Restaurant $category_Restaurant)
+    {
+        //
+    }
+}
