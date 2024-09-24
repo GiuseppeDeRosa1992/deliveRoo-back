4a05658c6202a6a430f0e1f15c81fbadbfd3a509
davidearbola <davidearbola@gmail.com>
2024-09-24 12:12:36 +0200

    fix categories method

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -16,12 +16,12 @@ class DashboardController extends Controller
         $restaurant = $user->restaurant;
         $orders = Order::where('restaurant_id', $restaurant->id)->orderByDesc('created_at')->get();
         $products = Product::where('restaurant_id', $restaurant->id)->orderBy('name')->get();
-        $categories = Category::where('restaurant_id', $restaurant->id)->orderBy('name')->get();
+        $categories = $restaurant->categories();
 
 
 
 
 
-        return view('admin.dashboard', compact('user', 'restaurant', 'orders', 'products'));
+        return view('admin.dashboard', compact('user', 'restaurant', 'orders', 'products', 'categories'));
     }
 }
