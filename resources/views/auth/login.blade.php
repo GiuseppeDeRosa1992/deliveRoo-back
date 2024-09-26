b98bae65b238fcf03fa7236e979c5f6c0155904a
francescofalcone24 <cicciofalcon97@gmail.com>
2024-09-26 12:49:57 +0300

    div vh-100 container

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,96 +1,98 @@
 @extends('layouts.app')
 
 @section('content')
-	<div class="container vh-100 pt-2 position-relative">
-		<div class="row justify-content-center m-0 logo">
-			{{-- **********LOGO******* --}}
-			<div class="col-5 py-lg-2 py-md-1">
-				<img class="img-fluid" src="{{ asset('img/Delive8.png') }}" alt="">
+	<div class="vh-100  position-relative">
+		<div class="container ">
+			<div class="row justify-content-center m-0 logo">
+				{{-- **********LOGO******* --}}
+				<div class="col-5 py-lg-2 py-md-1">
+					<img class="img-fluid" src="{{ asset('img/Delive8.png') }}" alt="">
+				</div>
+				{{-- **********LOGO******* --}}
 			</div>
-			{{-- **********LOGO******* --}}
-		</div>
-		<div class="row justify-content-center mt-2">
-			<div class="col-md-8">
-				<div class="card">
-					<div class="card-header">{{ __('Accedi') }}</div>
+			<div class="row justify-content-center mt-2">
+				<div class="col-md-8">
+					<div class="card">
+						<div class="card-header">{{ __('Accedi') }}</div>
 
-					<div class="card-body">
-						<form method="POST" action="{{ route('login') }}">
-							@csrf
+						<div class="card-body">
+							<form method="POST" action="{{ route('login') }}">
+								@csrf
 
-							<div class="mb-4 row">
-								<label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Indirizzo E-Mail') }}</label>
+								<div class="mb-4 row">
+									<label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Indirizzo E-Mail') }}</label>
 
-								<div class="col-md-6">
-									<input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email"
-										value="{{ old('email') }}" required autocomplete="email" autofocus>
+									<div class="col-md-6">
+										<input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email"
+											value="{{ old('email') }}" required autocomplete="email" autofocus>
 
-									@error('email')
-										<span class="invalid-feedback" role="alert">
-											<strong>{{ $message }}</strong>
-										</span>
-									@enderror
-								</div>
-							</div>
-
-							<div class="mb-4 row">
-								<label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-								<div class="col-md-6">
-									<input id="password" type="password" class="form-control @error('password') is-invalid @enderror"
-										name="password" required autocomplete="current-password">
-
-									@error('password')
-										<span class="invalid-feedback" role="alert">
-											<strong>{{ $message }}</strong>
-										</span>
-									@enderror
-								</div>
-							</div>
-
-							<div class="mb-4 row">
-								<div class="col-md-6 offset-md-4">
-									<div class="form-check">
-										<input class="form-check-input" type="checkbox" name="remember" id="remember"
-											{{ old('remember') ? 'checked' : '' }}>
-
-										<label class="form-check-label" for="remember">
-											{{ __('Ricordami') }}
-										</label>
+										@error('email')
+											<span class="invalid-feedback" role="alert">
+												<strong>{{ $message }}</strong>
+											</span>
+										@enderror
 									</div>
 								</div>
-							</div>
 
-							<div class="mb-4 row mb-0">
-								<div class="col-md-8 offset-md-4">
-									<button type="submit" class="btn btn-primary my_orange">
-										{{ __('Accedi') }}
-									</button>
+								<div class="mb-4 row">
+									<label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
-									@if (Route::has('password.request'))
-										<a class="btn btn-link" href="{{ route('password.request') }}">
-											{{ __('Hai Dimenticato la Password?') }}
-										</a>
-									@endif
-									<br>
-									@if (Route::has('register'))
-										<div class="mt-3">
-											<span>Non sei ancora registrato ? </span>
-											<a class="">
-												<a class="" href="{{ route('register') }}"> {{ __('Registrati') }}</a>
-											</a>
-										</div>
-									@endif
+									<div class="col-md-6">
+										<input id="password" type="password" class="form-control @error('password') is-invalid @enderror"
+											name="password" required autocomplete="current-password">
+
+										@error('password')
+											<span class="invalid-feedback" role="alert">
+												<strong>{{ $message }}</strong>
+											</span>
+										@enderror
+									</div>
 								</div>
-							</div>
-						</form>
+
+								<div class="mb-4 row">
+									<div class="col-md-6 offset-md-4">
+										<div class="form-check">
+											<input class="form-check-input" type="checkbox" name="remember" id="remember"
+												{{ old('remember') ? 'checked' : '' }}>
+
+											<label class="form-check-label" for="remember">
+												{{ __('Ricordami') }}
+											</label>
+										</div>
+									</div>
+								</div>
+
+								<div class="mb-4 row mb-0">
+									<div class="col-md-8 offset-md-4">
+										<button type="submit" class="btn btn-primary my_orange">
+											{{ __('Accedi') }}
+										</button>
+
+										@if (Route::has('password.request'))
+											<a class="btn btn-link" href="{{ route('password.request') }}">
+												{{ __('Hai Dimenticato la Password?') }}
+											</a>
+										@endif
+										<br>
+										@if (Route::has('register'))
+											<div class="mt-3">
+												<span>Non sei ancora registrato ? </span>
+												<a class="">
+													<a class="" href="{{ route('register') }}"> {{ __('Registrati') }}</a>
+												</a>
+											</div>
+										@endif
+									</div>
+								</div>
+							</form>
+						</div>
 					</div>
 				</div>
 			</div>
+
+
 		</div>
-
 		<img src="{{ asset('img/scooter-icon.png') }}" alt="" class="opacity-1  position-absolute ">
-
 	</div>
 @endsection
 
